b587356402a88542d4336cfc30251d0072bbeb6b
Julien <julien@ripouteau.com>
2021-11-12 17:02:45 +0100

    ✨ added lib.rs bundle

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+mod chart;
+use chart::*;
+
+mod chart_renderer;
+use chart_renderer::*;
+
+mod y_axis;
+use y_axis::*;
+
+mod chart_data;
+use chart_data::*;
+
+mod info_bar;
+
+pub use chart::Candle;
+pub use chart::Chart;
