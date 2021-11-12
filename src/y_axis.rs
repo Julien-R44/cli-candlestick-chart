08e64e7bf65383dfc38632055ea56e6764abe391
Julien <julien@ripouteau.com>
2021-11-12 15:02:32 +0100

    ✨ update y_axis rendering for handling more cases

--- src/y_axis.rs
+++ src/y_axis.rs
@@ -2,12 +2,22 @@ use crate::chart_data::ChartData;
 
 pub struct YAxis {
     chart_data: ChartData,
+    width: i64,
+    margin_right: i64,
+    max_len_characteristic: i64,
 }
 
 impl YAxis {
     pub fn new(chart_data: &ChartData) -> YAxis {
+        let cloned_chart_data = chart_data.clone();
+
+        let max_len_characteristic = chart_data.max_value.ceil().to_string().len() as i64;
+
         YAxis {
-            chart_data: chart_data.clone(),
+            chart_data: cloned_chart_data,
+            width: 5,
+            margin_right: 4,
+            max_len_characteristic,
         }
     }
 
@@ -23,7 +33,7 @@ impl YAxis {
     pub fn render_line(&self, y: u16) -> String {
         match y % 4 {
             0 => self.render_tick(y),
-            _ => self.render_empty(y),
+            _ => self.render_empty(),
         }
     }
 
@@ -32,15 +42,21 @@ impl YAxis {
         let max_value = self.chart_data.max_value;
         let height = self.chart_data.terminal_size.1;
 
-        let output = format!(
-            "{:.2} │┈   ",
-            min_value + (y as f64 * (max_value - min_value) / height as f64)
-        );
+        let price = min_value + (y as f64 * (max_value - min_value) / height as f64);
+        let cell_min_length = (self.max_len_characteristic + 3) as usize;
 
-        return output;
+        format!(
+            "{0:<cell_min_length$.2} │┈{margin}",
+            price,
+            cell_min_length = cell_min_length,
+            margin = " ".repeat(self.margin_right as usize)
+        )
     }
 
-    fn render_empty(&self, y: u16) -> String {
-        "       │    ".to_string()
+    fn render_empty(&self) -> String {
+        let cell = " ".repeat((self.max_len_characteristic + 4) as usize);
+        let margin = " ".repeat((self.margin_right + 1).try_into().unwrap());
+
+        format!("{}│{}", cell, margin)
     }
 }
